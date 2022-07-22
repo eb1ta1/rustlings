596cb520718d03055a0d00b57e2743641c929bdb
eb1ta1 <ebitaiume@gmail.ccom>
2022-07-22 16:26:19 +0900

    add

--- move_semantics/move_semantics3.rs
+++ move_semantics/move_semantics3.rs
@@ -3,8 +3,6 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
@@ -18,9 +16,10 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    let mut vec_clone = vec.clone();
+    vec_clone.push(22);
+    vec_clone.push(44);
+    vec_clone.push(66);
 
-    vec
+    vec_clone
 }
--- variables/variables5.rs
+++ variables/variables5.rs
@@ -2,7 +2,7 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let mut number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
     number = "3"; // don't rename this variable
     println!("Number plus two is : {}", number.parse::<u8>().unwrap() + 2);
